249fb07f99a1dcbe68e4621b92889339d983a360
Sojharo <sojharo@live.com>
2016-11-09 03:18:10 +0500

    connect middleware 4, agent assignment 5

--- server/auth/local/passport.js
+++ server/auth/local/passport.js
@@ -23,3 +23,46 @@ exports.setup = function (User, config) {
     }
   ));
 };
+
+function isAuthenticated() {
+  return compose()
+    // Validate jwt
+    .use(function(req, res, next) {
+      if(req.headers.hasOwnProperty('kibo-app-id')){
+        if(req.headers['kibo-app-id'] === '5wdqvvi8jyvfhxrxmu73dxun9za8x5u6n59'){
+          console.log('app id is known');
+          if(req.headers['kibo-app-secret'] === 'jcmhec567tllydwhhy2z692l79j8bkxmaa98do1bjer16cdu5h79xvx'){
+            console.log('client secret is correct')
+
+            User.findOne({uniqueid: req.headers['kibo-client-id'], isAdmin: 'Yes'}, function(err, user){
+              if(err) return next(err);
+              if(user) {
+                req.user = user;
+                next();
+              } else {
+                console.log('client app is not authorized');
+                return res.send(401);
+              }
+            })
+
+          }
+        }
+      } else {
+        // allow access_token to be passed through query parameter as well
+        if(req.query && req.query.hasOwnProperty('access_token')) {
+          req.headers.authorization = 'Bearer ' + req.query.access_token;
+        }
+        validateJwt(req, res, next);
+      }
+    })
+    // Attach user to request
+    .use(function(req, res, next) {
+      User.findById(req.user._id, function (err, user) {
+        if (err) return next(err);
+        if (!user) return res.send(401);
+
+        req.user = user;
+        next();
+      });
+    });
+}
