8b929ee5da527afb5b7a69674c9887b3e10d1f31
Sojharo <sojharo@live.com>
2017-05-29 12:18:30 +0500

    css in email verification and password reset 477 478

--- dist/server/api/user/user.controller.js
+++ dist/server/api/user/user.controller.js
@@ -835,7 +835,16 @@ exports.requestpasswordchangeKiboEngage = function(req, res) {
         text:     'Password Reset'
       });
 
-      email.setHtml('<h1>KiboEngage</h1><br><br>Use the following link to change your password <br><br> https://kiboengage.kibosupport.com/resetpassword/'+ tokenString);
+      email2.setHtml('<body style="min-width: 80%;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;margin: 0;padding: 0;direction: ltr;background: #f6f8f1;width: 80% !important;"><table class="body", style="width:100%"> ' +
+              '<tr> <td class="center" align="center" valign="top"> <!-- BEGIN: Header --> <table class="page-header" align="center" style="width: 100%;background: #1f1f1f;"> <tr> <td class="center" align="center"> ' +
+              '<!-- BEGIN: Header Container --> <table class="container" align="center"> <tr> <td> <table class="row "> <tr>  </tr> </table> <!-- END: Logo --> </td> <td class="wrapper vertical-middle last" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;"> <!-- BEGIN: Social Icons --> <table class="six columns"> ' +
+              '<tr> <td> <table class="wrapper social-icons" align="right" style="float: right;"> <tr> <td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;"> ' +
+              '<p style="color: #ffffff"> KiboEngage - Reset Password </p> </td></tr> </table> </td> </tr> </table> ' +
+              '<!-- END: Social Icons --> </td> </tr> </table> </td> </tr> </table> ' +
+              '<!-- END: Header Container --> </td> </tr> </table> <!-- END: Header --> <!-- BEGIN: Content --> <table class="container content" align="center"> <tr> <td> <table class="row note"> ' +
+              '<tr> <td class="wrapper last"> <p> Hello, <br> This is to inform you that you have requested to change your password for you KiboEngage account </p> <p> </p>  <!-- BEGIN: Note Panel --> <table class="twelve columns" style="margin-bottom: 10px"> ' +
+              '<tr> <td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;"> </td> <td class="expander"> </td> </tr> </table> <p> Use the following link to change your password <br><br> https://kiboengage.kibosupport.com/resetpassword/'+ tokenString +' </p> <!-- END: Note Panel --> </td> </tr> </table><span class="devider" style="border-bottom: 1px solid #eee;margin: 15px -15px;display: block;"></span> <!-- END: Disscount Content --> </td> </tr> </table> </td> </tr> </table> <!-- END: Content --> <!-- BEGIN: Footer --> <table class="page-footer" align="center" style="width: 100%;background: #2f2f2f;"> <tr> <td class="center" align="center" style="vertical-align: middle;color: #fff;"> <table class="container" align="center"> <tr> <td style="vertical-align: middle;color: #fff;"> <!-- BEGIN: Unsubscribet --> <table class="row"> <tr> <td class="wrapper last" style="vertical-align: middle;color: #fff;"><span style="font-size:12px;"><i>This ia a system generated email and reply is not required.</i></span> </td> </tr> </table> <!-- END: Unsubscribe --> ' +
+              '<!-- END: Footer Panel List --> </td> </tr> </table> </td> </tr> </table> <!-- END: Footer --> </td> </tr></table></body>')
 
       sendgrid.send(email, function(err, json) {
         if (err) { return console.error(err); }
@@ -1284,16 +1293,13 @@ exports.createKiboEngageUser = function (req, res, next) {
               '<tr> <td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;"> <a href="https://kiboengage.kibosupport.com/verification/'+ tokenString +'">https://kiboengage.kibosupport.com/verification/'+ tokenString +'</a> </td> <td class="expander"> </td> </tr> </table> <p> If clicking the URL above does not work, copy and paste the URL into a browser window. </p> <!-- END: Note Panel --> </td> </tr> </table><span class="devider" style="border-bottom: 1px solid #eee;margin: 15px -15px;display: block;"></span> <!-- END: Disscount Content --> </td> </tr> </table> </td> </tr> </table> <!-- END: Content --> <!-- BEGIN: Footer --> <table class="page-footer" align="center" style="width: 100%;background: #2f2f2f;"> <tr> <td class="center" align="center" style="vertical-align: middle;color: #fff;"> <table class="container" align="center"> <tr> <td style="vertical-align: middle;color: #fff;"> <!-- BEGIN: Unsubscribet --> <table class="row"> <tr> <td class="wrapper last" style="vertical-align: middle;color: #fff;"><span style="font-size:12px;"><i>This ia a system generated email and reply is not required.</i></span> </td> </tr> </table> <!-- END: Unsubscribe --> ' +
               '<!-- END: Footer Panel List --> </td> </tr> </table> </td> </tr> </table> <!-- END: Footer --> </td> </tr></table></body>')
 
-
-            email.setHtml('<h1>KiboEngage</h1><br><br>Use the following link to verify your account <br><br> <a href="https://kiboengage.kibosupport.com/verification/'+ tokenString +'"> https://kiboengage.kibosupport.com/verification/'+ tokenString +'</a>');
-
             sendgrid.send(email, function(err, json) {
               if (err) { return console.log(err); }
               //console.log(json);
             });
 
             var email2     = new sendgrid.Email({
-              to:       'shabir.saba@gmail.com',
+              to:       'sojharo@gmail.com',
               from:     'support@cloudkibo.com',
               subject:  'KiboEngage: Account created by '+ req.body.website.toLowerCase(),
               text:     'Welcome to KiboEngage'
@@ -1536,7 +1542,16 @@ exports.reapplyverificationlinkKiboEngage = function(req, res) {
       text:     'Welcome to KiboEngage'
     });
 
-    email.setHtml('<h1>KiboEngage</h1><br><br>Use the following link to verify your account <br><br> https://kiboengage.kibosupport.com/verification/'+ tokenString);
+    email.setHtml('<body style="min-width: 80%;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;margin: 0;padding: 0;direction: ltr;background: #f6f8f1;width: 80% !important;"><table class="body", style="width:100%"> ' +
+              '<tr> <td class="center" align="center" valign="top"> <!-- BEGIN: Header --> <table class="page-header" align="center" style="width: 100%;background: #1f1f1f;"> <tr> <td class="center" align="center"> ' +
+              '<!-- BEGIN: Header Container --> <table class="container" align="center"> <tr> <td> <table class="row "> <tr>  </tr> </table> <!-- END: Logo --> </td> <td class="wrapper vertical-middle last" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;"> <!-- BEGIN: Social Icons --> <table class="six columns"> ' +
+              '<tr> <td> <table class="wrapper social-icons" align="right" style="float: right;"> <tr> <td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;"> ' +
+              '<p style="color: #ffffff">Verify your account</p> </td></tr> </table> </td> </tr> </table> ' +
+              '<!-- END: Social Icons --> </td> </tr> </table> </td> </tr> </table> ' +
+              '<!-- END: Header Container --> </td> </tr> </table> <!-- END: Header --> <!-- BEGIN: Content --> <table class="container content" align="center"> <tr> <td> <table class="row note"> ' +
+              '<tr> <td class="wrapper last"> <p> Hello, <br> Thank you for joining KiboEngage. <br>Use the following link to verify your account <br>  </p> <p>To accept invitation please click the following URL to activate your account:</p> <!-- BEGIN: Note Panel --> <table class="twelve columns" style="margin-bottom: 10px"> ' +
+              '<tr> <td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;"> <a href="https://kiboengage.kibosupport.com/verification/'+ tokenString +'">https://kiboengage.kibosupport.com/verification/'+ tokenString +'</a> </td> <td class="expander"> </td> </tr> </table> <p> If clicking the URL above does not work, copy and paste the URL into a browser window. </p> <!-- END: Note Panel --> </td> </tr> </table><span class="devider" style="border-bottom: 1px solid #eee;margin: 15px -15px;display: block;"></span> <!-- END: Disscount Content --> </td> </tr> </table> </td> </tr> </table> <!-- END: Content --> <!-- BEGIN: Footer --> <table class="page-footer" align="center" style="width: 100%;background: #2f2f2f;"> <tr> <td class="center" align="center" style="vertical-align: middle;color: #fff;"> <table class="container" align="center"> <tr> <td style="vertical-align: middle;color: #fff;"> <!-- BEGIN: Unsubscribet --> <table class="row"> <tr> <td class="wrapper last" style="vertical-align: middle;color: #fff;"><span style="font-size:12px;"><i>This ia a system generated email and reply is not required.</i></span> </td> </tr> </table> <!-- END: Unsubscribe --> ' +
+              '<!-- END: Footer Panel List --> </td> </tr> </table> </td> </tr> </table> <!-- END: Footer --> </td> </tr></table></body>')
 
     sendgrid.send(email, function(err, json) {
       if (err) { return console.log(err); }
