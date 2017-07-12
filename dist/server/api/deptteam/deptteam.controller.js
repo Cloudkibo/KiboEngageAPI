82b3584a2d07ed8d94ded41a553347d5e988aad6
Sojharo <sojharo@live.com>
2017-07-12 13:06:48 +0500

    server endpoint 222

--- dist/server/api/deptteam/deptteam.controller.js
+++ dist/server/api/deptteam/deptteam.controller.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 var Deptteam = require('./deptteam.model');
 var User = require('../user/user.model');
 var groupagent = require('../groupagent/groupagent.model');
+var logger = require('../../components/logger/logger');
 
 // Get list of deptteams
 exports.index = function(req, res) {
@@ -29,19 +30,39 @@ Deptteam.find({companyid : req.user.uniqueid,deleteStatus : 'No'}).populate('dep
 exports.getallAgents = function(req, res) {
   Deptteam.find({companyid : req.user.uniqueid, deptid: req.params.id, deleteStatus : 'No'}, function (err, deptteams) {
       if(err) { return handleError(res, err); }
+      logger.serverLog('info', 'going to get all the agents route for a department');
+      console.log('going to get all the agents route for a department');
+
+      logger.serverLog('info', deptteams);
+      console.log(deptteams);
+
       var teams = [];
 
       for(var i in deptteams) {
         teams.push(deptteams[i].teamid);
       }
 
+      logger.serverLog('info', 'these teams are in this deparment');
+      console.log('these teams are in this department');
+
+      logger.serverLog('info', teams);
+      console.log(teams);
+
       groupagent.find({groupid : {$in : teams}}).populate('agentid').exec(function(err3, agentsgroup){
         if(err3) { return handleError(res, err3); }
+
+        logger.serverLog('info', 'these are agents in the teams');
+        console.log('these are agents in the teams');
+
+        logger.serverLog('info', agentsgroup);
+        console.log(agentsgroup);
+
         var agents = [];
 
         for(var i in agentsgroup){
           agents.push(agentsgroup[i].agentid);
         }
+
         return res.json(200, agents);
       })
 
